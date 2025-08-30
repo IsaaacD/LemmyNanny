07fa12e5efb35622a27bd1dea2d8d4adf13e1e5d
Isaac Dow <isaac@merbs.ca>
2025-08-30 12:37:53 -0400

    need to fix dotnetlemmy reference

---
+++ LemmyNanny/LemmyWorker.cs
@@ -0,0 +1,212 @@
+﻿using dotNETLemmy.API.Types;
+using dotNETLemmy.API.Types.Forms;
+using dotNETLemmy.API.Types.Responses;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Hosting;
+using OllamaSharp;
+using OllamaSharp.Models.Chat;
+using SixLabors.ImageSharp;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LemmyNanny
+{
+    public class LemmyWorker : BackgroundService
+    {
+        public static string HintTextColor { get; } = "gray";
+
+        public static string AccentTextColor { get; } = "blue";
+
+        public static string WarningTextColor { get; } = "yellow";
+
+        public static string ErrorTextColor { get; } = "red";
+
+        public static string AiTextColor { get; } = "cyan";
+        public static string AiThinkTextColor { get; } = "gray";
+
+        public string BaseAddress
+        {
+            get => _lemmyHttpClient.BaseAddress;
+            set => _lemmyHttpClient.BaseAddress = value;
+        }
+
+        public string SqliteConnection { get; set; }
+        public string LemmyUserName { get; set; }
+        public string LemmyPassword { get; set; }
+
+        public string OllamaUrl { get; set; }
+        public string OllamaModel { get; set; }
+        private HttpClient _picsHttpClient = new HttpClient();
+
+        private readonly ILemmyHttpClient _lemmyHttpClient;
+
+        private string? _lastPage = string.Empty;
+        private List<ProcessedPost> _seenPosts = new();
+
+        public LemmyWorker(ILemmyHttpClient lemmyHttpClient) =>
+            _lemmyHttpClient = lemmyHttpClient;
+
+        public class ProcessedPost
+        {
+            public int PostId { get; set; }
+            public bool Reported { get; set; }
+            public string? Reason { get; set; }
+        }
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var ollamaHttpClient = new HttpClient() { BaseAddress = new Uri(OllamaUrl), Timeout= TimeSpan.FromMinutes(10) };
+            var ollama = new OllamaApiClient(ollamaHttpClient);
+            ollama.SelectedModel = OllamaModel;
+            
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var form = new GetPostsForm();
+                if (!string.IsNullOrEmpty(_lastPage)) { 
+                    form.PageCursor = _lastPage;
+                    AnsiConsole.WriteLine($"set form.PageCursor={_lastPage}");
+                }
+                var getPostsResponse = await _lemmyHttpClient.GetPosts(form, cancellationToken);
+                _lastPage = getPostsResponse.NextPage;
+                AnsiConsole.WriteLine($"Checking {getPostsResponse.Posts.Length} posts");
+                foreach (var postView in getPostsResponse.Posts)
+                {
+                    var post = postView;
+                    var firstSeen = _seenPosts.FirstOrDefault(o => o.PostId == post.Post.Id);
+                    if (firstSeen != null)
+                    {
+                        AnsiConsole.WriteLine($"Already seen post #{post.Post.Id}: {firstSeen.Reason}");
+                    }
+                    var newSeen = new ProcessedPost { PostId = post.Post.Id };
+                    _seenPosts.Add(newSeen);
+                    AnsiConsole.WriteLine("");
+                    var postPrompt = $@"You are a moderator on a social media forum, the following is a post that needs to be vetted for community guideline violations. Please output only 'Yes' or 'No' as the answer. If the answer is 'Yes', expand on what the guideline violation could be.";
+                    var chat = new Chat(ollama, postPrompt) { Think = false };
+                    IAsyncEnumerable<string>? chatResults = null;
+                    var postInfo = $"PostId:{post.Post.Id}\r\nTitle: {post.Post.Name}\r\nBody: {post.Post.Body}";
+                    AnsiConsole.WriteLine(postInfo);
+                    var alreadySeen = false;
+                    using (var connection = new SqliteConnection(SqliteConnection))
+                    {
+                        connection.Open();
+
+                        var command = connection.CreateCommand();
+                        command.CommandText =
+                        @"
+                            SELECT timestamp
+                            FROM seen_posts
+                            WHERE post_id = $id
+                        ";
+                        command.Parameters.AddWithValue("$id", post.Post.Id);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // exists
+                                var timestamp = reader.GetString(0);
+                                AnsiConsole.WriteLine($"Already seen post {post.Post.Id} @ {timestamp}. Skipping to next one.");
+                                alreadySeen = true;
+                            }
+                        }
+                    }
+                    if (!alreadySeen)
+                    {
+                        try
+                        {
+                            if (post.Post?.Url?.Contains("/pictrs/") ?? false)
+                            {
+                                var imageBytes = new[] { await _picsHttpClient.GetByteArrayAsync(post.Post.Url) };
+                                AnsiConsole.WriteLine("The following image is compressed for console view, full image goes to the model.");
+                                //AnsiConsole.Write("The following image is compressed for console view, full image goes to the model.");
+                                foreach (var consoleImage in imageBytes.Select(bytes => new CanvasImage(bytes)))
+                                {
+                                    consoleImage.MaxWidth = 40;
+                                    AnsiConsole.Write(consoleImage);
+                                }
+                                chatResults = chat.SendAsync(postInfo, imageBytes, cancellationToken);
+                            }
+                            else
+                            {
+                                chatResults = chat.SendAsync(postInfo, cancellationToken);
+                            }
+
+
+                        }
+                        catch (UnknownImageFormatException)
+                        {
+                            AnsiConsole.WriteLine("");
+                            AnsiConsole.MarkupInterpolated($"[{ErrorTextColor}]*** post.Post.Url={post?.Post?.Url}. Cannot process UnknownImageFormatException. Likely type failure. ***[/]");
+                            AnsiConsole.WriteLine("");
+                            chatResults = chat.SendAsync(postInfo, cancellationToken);
+                        }
+                        catch (Exception e)
+                        {
+                            AnsiConsole.WriteLine($"Failed {e.GetType()}");
+
+                        }
+
+                        var resultOutput = new StringBuilder();
+                        if (chatResults == null)
+                        {
+                            return;
+                        }
+                        await foreach (var answerToken in chatResults)
+                        {
+                            resultOutput.Append(answerToken);
+                            AnsiConsole.MarkupInterpolated($"[{AiTextColor}]{answerToken}[/]");
+                        }
+                        var isNo = resultOutput.ToString().StartsWith("No");
+                        newSeen.Reported = !isNo;
+                        newSeen.Reason = resultOutput.ToString();
+                        AnsiConsole.WriteLine("");
+                        if (isNo)
+                        {
+                            AnsiConsole.WriteLine($"Found 'No', did not report {post?.Post?.Id}.");
+                        }
+                        else
+                        {
+                            AnsiConsole.WriteLine($"Found 'Yes', time to report {post?.Post?.Id ?? 0} with resultOutput={resultOutput}");
+
+
+                            var loginForm = new LoginForm
+                            {
+                                UsernameOrEmail = LemmyUserName,
+                                Password = LemmyPassword
+                            };
+
+                            var loginResponse = await _lemmyHttpClient.Login(loginForm);
+
+                            var report = new CreatePostReportForm() { Auth = loginResponse.Jwt, PostId = post.Post.Id, Reason = resultOutput.ToString() };
+                            var resp = await _lemmyHttpClient.CreatePostReport(report);
+                            AnsiConsole.WriteLine($"Reported {post?.Post?.Id ?? 0}.");
+                        }
+
+                        using (var connection = new SqliteConnection(SqliteConnection))
+                        {
+                            connection.Open();
+                            var command2 = connection.CreateCommand();
+                            command2.CommandText =
+                                    @$"
+                            INSERT INTO seen_posts (post_id, url, remarks, timestamp)
+                            VALUES ($id, $url, $remarks, $timestamp);
+                        ";
+                            command2.Parameters.AddWithValue("$id", post.Post.Id);
+                            command2.Parameters.AddWithValue("$url", post.Post.ApId);
+                            command2.Parameters.AddWithValue("$remarks", resultOutput.ToString());
+                            command2.Parameters.AddWithValue("$timestamp", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+                            command2.ExecuteNonQuery();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LemmyNanny/Program.cs
@@ -0,0 +1,33 @@
+﻿using dotNETLemmy.API;
+using dotNETLemmy.API.Types;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LemmyNanny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddHostedService(provider =>
+                new LemmyWorker(provider.GetRequiredService<ILemmyHttpClient>())
+                {
+                    BaseAddress = builder.Configuration["LemmyHost"] ?? throw new Exception("LemmyHost not set"),
+                    LemmyUserName = builder.Configuration["LemmyUserName"] ?? throw new Exception("LemmyUserName not set"),
+                    LemmyPassword = builder.Configuration["LemmyPassword"] ?? throw new Exception("LemmyPassword not set"),    
+                    OllamaModel = builder.Configuration["OllamaModel"] ?? throw new Exception("OllamaModel not set"),
+                    OllamaUrl = builder.Configuration["OllamaUrl"] ?? throw new Exception("OllamaUrl not set"),
+                    SqliteConnection = builder.Configuration["SqliteDb"] ?? throw new Exception("SqliteDb not set")
+
+                });
+
+            builder.Services.AddHttpClient<ILemmyHttpClient, LemmyHttpClient>();
+
+            IHost host = builder.Build();
+
+            host.Run();
+        }
+    }
+}
